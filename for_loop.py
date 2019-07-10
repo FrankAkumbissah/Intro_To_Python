455a2c1e167c2f16578b02c1efd60cd43bf11e3d
FrankAkumbissah <akumbissahfrank@gmail.com>
2019-07-10 15:15:11 +0000

    second commit

---
+++ for_loop.py
@@ -0,0 +1,37 @@
+for  i in range(1, 11):
+    print (i)
+#it created a for loop with the range starting from 1 and ending at 11
+#then we printed the values within the loop
+
+for j in [1, 2, 3]:
+    print(j)
+#we created a list and using a for loop, displayed the elements of the loop
+
+#this code with range does the same thing
+for i in range(1, 4):
+    print (i)
+    
+#[1, 2, 3] this type of data is a list
+#range returns integers, or elements in a list
+
+for i in range(0, 20):
+   i = i + 2
+   print (i)
+   
+
+def stars():
+    i = 4
+    while(i > 0):
+        print ("****")
+        i = i - 1
+    
+
+def stars2():
+    for i in range (0, 4):
+        for j in range(0, i + 1):
+            print("*", end = ' ')
+        print("\n")
+    for i in range (0, 4):
+        for j in range(i + 1, 4):
+            print("*", end = ' ')
+        print("\n")
